08b1d504ea98ca3781d2849b12e36a8c0a3a59f1
Matthew Holder <matt@slimwareutilities.com>
2016-05-09 06:43:33 -0500

    Created MMC Snap-in Project

---
+++ ntUSPd.Admin/AdminSnapInController.cs
@@ -0,0 +1,22 @@
+﻿namespace ntUSPd.Admin
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.ManagementConsole;
+
+	[SnapInSettings(
+		"{2FA4F7B3-C42C-44C9-A08E-A147D059B612}",
+		DisplayName = "Network UPS Tools Services",
+		Description = "Manages ntUPSd service")]
+	public class AdminSnapInController : SnapIn
+	{
+		public AdminSnapInController()
+		{
+			this.RootNode = new ScopeNode();
+			this.RootNode.DisplayName = "Network UPS Services";
+		}
+	}
+}
---
+++ ntUSPd.Admin/AdminSnapInInstaller.cs
@@ -0,0 +1,15 @@
+﻿namespace ntUSPd.Admin
+{
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Microsoft.ManagementConsole;
+
+	[RunInstaller(true)]
+	public class AdminSnapInInstaller : SnapInInstaller
+	{
+	}
+}
